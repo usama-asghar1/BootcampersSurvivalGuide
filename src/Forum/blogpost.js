94059b35f945408709882bb116b861895ba8ea07
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-04-26 14:46:22 +0100

    resolved conflict w topics

--- src/Forum/blogpost.js
+++ src/Forum/blogpost.js
@@ -1,26 +1,15 @@
-/*
-export default function BlogPost({ title, topics }) {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <p>Topics covered this week:</p>
-      <ul> 
+export default function BlogPost({title, topics}) {
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>Topics covered this week: </p>
+
+        <ul> 
       <li>{topics[0]}</li>
       <li>{topics[1]}</li>
       <li>{topics[2]}</li>
     
       </ul>
-    </div>
-  );
-}
-*/
-
-export default function BlogPost({title, topics}) {
-    return (
-      <div>
-        <h1>{title}</h1>
-        <p>Topics covered this week: <br/>
-        {topics}</p>
         
       </div>
     )
