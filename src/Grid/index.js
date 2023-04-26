6a6b29daa31c802a16a02c550c6bce2a03fc6f3b
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-04-26 11:38:11 +0100

    made 4x4 grid

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,10 +1,12 @@
 import {Link} from "react-router-dom";
+import React from "react";
+import "./style.css";
 
 export default function Grid (props) {
 
   const gridList = props.data.map(grid => (
     <div>
-    <button key={grid.id}>
+    <button className = "grid-cell" key={grid.id}>
     <Link to={`/forums/${grid.id}`}>Week {grid.id}</Link>
     </button>
      </div>
@@ -12,7 +14,11 @@ export default function Grid (props) {
      
   return (
     <div>
+    <h1 className = "grid-title">School of Code Survival Guide</h1>
+    <h2 className = "grid-subtitle">Click on a week to see the forum</h2>
+    <div className = "grid-container">
 {gridList}
+    </div>
     </div>
   )
 }
