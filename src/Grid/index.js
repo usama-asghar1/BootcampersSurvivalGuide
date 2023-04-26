f961b1c58cf7995b3e876e00da290683f127dee7
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-04-26 21:02:40 +0100

    hover works

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -6,31 +6,61 @@ export default function Grid (props) {
   
   const gridList = props.data.map(grid => (
     <div>
+    <Link to={`/forums/${grid.id}`}>
     <button className = "grid-cell" key={grid.id}>
-    <Link to={`/forums/${grid.id}`}>Week {grid.id}</Link>
+    <h1 className="week-list"> Week {grid.id} </h1>
     <ul className="topic-list">
-          <li>Topic 1</li>
-          <li>Topic 2</li>
-          <li>Topic 3</li>
+          <li>{grid.topics[0]}</li>
+          <li>{grid.topics[1]}</li>
+          <li>{grid.topics[2]}</li>
         </ul>
     </button>
+    </Link>
      </div>
      ));
      
   return (
-    <div>
+  <div>
     <h1 className = "grid-title">Bootcamper's Survival Guide</h1>
     <h2 className = "grid-subtitle">Click on a week to see the forum</h2>
     <div className = "grid-container">
-{gridList}
+    {gridList}
     </div>
-    </div>
+  </div>
   )
 }
 
-//  import React from "react";
-// import Square from "../Square";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import "./style.css";
 
+// export default function Grid(props) {
+//   const navigate = useNavigate();
 
-//   return <div>{gridList}</div>;
+//   const handleWeekClick = (weekId) => {
+//     navigate(`/forums/${weekId}`);
+//   };
+
+//   const gridList = props.data.map((grid) => (
+//     <div key={grid.id} onClick={() => handleWeekClick(grid.id)}>
+//       <div className="grid-cell">
+//         <h1 className="week-list"> Week {grid.id} </h1>
+//         <ul className="topic-list">
+//           <li>{grid.topics[0]}</li>
+//           <li>{grid.topics[1]}</li>
+//           <li>{grid.topics[2]}</li>
+//         </ul>
+//       </div>
+//     </div>
+//   ));
+
+//   return (
+//     <div>
+//       <h1 className="grid-title">Bootcamper's Survival Guide</h1>
+//       <h2 className="grid-subtitle">
+//         Click on a week to see the forum
+//       </h2>
+//       <div className="grid-container">{gridList}</div>
+//     </div>
+//   );
 // }
