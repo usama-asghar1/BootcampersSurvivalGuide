3626255d09470c44d89366845d4a51c43d4556cd
anjaliruth <ruthanjali0211@gmail.com>
2023-04-27 00:02:28 +0800

    Space background with black borders

--- src/Forum/blogpost.js
+++ src/Forum/blogpost.js
@@ -19,13 +19,15 @@ export default function BlogPost({title, topics}) {
     return (
       <div className = "blogTitle">
         <h1>{title}</h1>
+        <div className = "topicsList">
         <p>Topics covered this week: </p>
         
-        <ul className = "topicsList">
+        <ul className="list">
           <li>{topics[0]}</li>
           <li>{topics[1]}</li>
           <li>{topics[2]}</li>
         </ul>
+        </div>
         
       </div>
     )
