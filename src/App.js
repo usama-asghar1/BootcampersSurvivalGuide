1349d13f37a052541786a3db0343281013f75271
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-04-27 13:56:45 +0100

    added title

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import {Routes, Route} from "react-router-dom";
 import Grid from "./Grid/index.js";
 import weekGrid from "./lib/data.js";
 import Login from "./Login/index.js";
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Bootcamper's Survival Guide";
+  }, []);
+  
   return (
-    <div>
+    <div>    
     <Routes>
       <Route path = "/" element = {<Login/>}/>
       <Route path = "/grid" element = {<Grid data = {weekGrid} />}/>
