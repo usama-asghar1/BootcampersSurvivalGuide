ff3429aae39655d46bd8dcf760aeaaa838a60cd3
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-04-27 21:56:00 +0100

    changed forum css except input stuff

--- src/Forum/commentlist.js
+++ src/Forum/commentlist.js
@@ -2,12 +2,15 @@ import Comment from "./comment.js";
 
 function CommentList({comments, data}) {
   return (
-    <div id="CommentList">
+    <div className = "forumSection" >
     {/* loop over the comments array and give each comment the prop of id, author and content */}
     <p className = "forumTitle">Forum for this week:</p>
+    <div id="CommentList">
     {comments.map((comment)=> 
     <Comment key={comment.id} author={comment.author} content={comment.content} />
-    )}</div>)
+    )}
+    </div>
+    </div>)
 
 }
 
