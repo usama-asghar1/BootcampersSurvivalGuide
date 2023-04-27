12e9ca4bd475309c11649b30533588ea818d93c1
JFord <jamesford173@yahoo.com>
2023-04-27 11:48:42 +0100

    add css styles to login page

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,24 +1,30 @@
 import {Link} from "react-router-dom";
+import './index.css';
 
 
 function Login() {
-
-
     return (
-        <div>
-            <h1>Login</h1>
-            <form>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" /><br /><br />
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" /><br /><br />
-                <Link to="/grid">
-                <button type="submit" value="Submit" >Submit</button>
-                </Link>
-            </form>
-            
-        </div>
-    )
-}
+     <div>
+        <h1 className = "grid-title">Bootcamper's Survival Guide</h1>
+    
 
-export default Login;
+      <div className="container">
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+          <br /><br />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <br /><br />
+          <Link to="/grid">
+            <button type="submit" value="Submit">Submit</button>
+          </Link>
+        </form>
+      </div>
+    </div>
+    );
+  }
+  
+  export default Login;
+  
