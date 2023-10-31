c43c9e45616bccb738641b39719f957d4df0207d
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-10-31 15:19:31 +0000

    commented out login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import Forum from './Forum/index.js';
 import {Routes, Route} from "react-router-dom";
 import Grid from "./Grid/index.js";
 import weekGrid from "./lib/data.js";
-import Login from "./Login/index.js";
+// import Login from "./Login/index.js";
 import { useEffect } from 'react';
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
   return (
     <div>    
     <Routes>
-      <Route path = "/" element = {<Login/>}/>
-      <Route path = "/grid" element = {<Grid data = {weekGrid} />}/>
+      {/* <Route path = "/" element = {<Login/>}/> */}
+      <Route path = "/" element = {<Grid data = {weekGrid} />}/>
       <Route path = "/forums/:id" element = {<Forum data = {weekGrid}/>}/>
       <Route/>
     </Routes>
